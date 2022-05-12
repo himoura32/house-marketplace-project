856b3b43e536b0e32593da464a73fa30cb531f52
himoura32 <shawn.ahearn@outlook.com>
2022-05-12 10:56:24 -0400

    coded pages/routes and navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-
+import  { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Explore from './pages/Explore';
+import Offer from './pages/Offer';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-    <h1>My App</h1>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Explore />} />
+        <Route path='/offer' element={<Offer />} />
+        <Route path='/profile' element={<SignIn />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/forgot-Password' element={<ForgotPassword />} />
+      </Routes>
+      <Navbar />
+    </Router>
     </>
   );
 }
