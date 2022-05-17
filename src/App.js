761721b25f7f65edad1981b554bc4e93b13b3192
himoura32 <shawn.ahearn@outlook.com>
2022-05-16 20:51:32 -0400

    coded contact landlord, coded leaflet map

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import CreateListing from './pages/CreateListing';
 import Listing from './pages/Listing';
+import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-Password' element={<ForgotPassword />} />
           <Route path='/create-listing' element={<CreateListing />} />
+          <Route path='/contact/:landlordId' element={<Contact />} />
           <Route
             path='/category/:categoryName/:listingId'
             element={<Listing />}
